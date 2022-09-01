2ed9121dacb85c39d84ac5a4b224e552ed1d1d6c
PatrickSara-Insight <patrick.sara@insight.com>
2022-09-01 16:16:07 +0800

    adjusted card padding

--- insight-demo/src/components/common/ImpactCard.jsx
+++ insight-demo/src/components/common/ImpactCard.jsx
@@ -8,8 +8,8 @@ const ImpactCard = (props) => {
       >
         {props.svg ? <img className="impact-svg" src={props.svg} alt="" /> : ""}
       </div>
-      <div className="flex flex-1 items-center justify-between rounded-r-md p-10 bg-white">
-        <div className="flex-1 px-4 py-2 text-sm">
+      <div className="flex flex-1 items-center justify-between rounded-r-md p-8 bg-white">
+        <div className="flex-1 px-2 py-0 text-sm">
           <dd className="mt-2 slide-stat-primary gray text-center">
             {props.stat}
           </dd>
